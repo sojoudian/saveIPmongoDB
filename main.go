410cba6d1b9fa40fea5e6f9138de40312ca13e6b
Maziar Sojoudian <maziar@sojoudian.net>
2024-03-21 17:44:10 -0400

    make connection to mongodb

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
+import (
+	"context"
+	"log"
 
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
-func main()  {
-  
+var mongoClient *mongo.Client
+
+func main() {
+	var err error
+	mongoClient, err = mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
 }
